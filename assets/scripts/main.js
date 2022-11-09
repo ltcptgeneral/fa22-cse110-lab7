d51c3f312f2d10e1b9ac69f0367d52efbf3b01b2
Arthur Lu <learthurgo@gmail.com>
2022-11-08 16:42:05 -0800

    fix scope issue with SW

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,7 +52,7 @@ function initializeServiceWorker() {
       // B3. TODO - Register '/sw.js' as a service worker (The MDN article
       //            "Using Service Workers" will help you here)
       try {
-        const registration = navigator.serviceWorker.register("/sw.js", {scope: "/",});
+        const registration = navigator.serviceWorker.register("./sw.js", {scope: "./",});
         // B4. TODO - Once the service worker has been successfully registered, console
         //            log that it was successful.
         console.log('service worker registration successful');
